2583437d33a2622aca3e81a3822d322123076fd3
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-01-24 07:28:00 -0500

    Add the template version of the DisplayArtwork.

---
+++ src/main/java/practicaltwo/DisplayArtwork.java
@@ -0,0 +1,28 @@
+package practicaltwo;
+
+// TODO: Import the Data class in the java.util package
+
+/** This class has a method that displays ASCII artwork.
+ * The first line displays the name of the programmer and the date. Then, the
+ * program's main method displays nine lines of artwork that use characters. The
+ * program should produce the ten total lines of output by using only a total of
+ * nine println statements. Please refer to the README.md file for more details
+ * about a special symbol that must also appear in the output of your program.
+ *
+ * <p>Bugs: This program has no known bugs, but is missing source code.
+ *
+ * @author Add Your Name Here
+ */
+
+public class DisplayArtwork {
+
+  // TODO: Add the correct type of multiple-line comment
+  public static void main(String[] args) {
+    // display the name of the programmer and the date
+    System.out.println("Add Your Name Here" + new Date());
+    // TODO: display the ten lines of the artwork
+    // while only using a total of nine println statements.
+    // Please see the assignment sheet for more details
+  }
+
+}
